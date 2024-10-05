f92e7198f5ead1b52d77f6e8f1bfc835e31c354f
gabriel-vincius-silva <gabreuvinici@gmail.com>
2024-10-05 20:37:22 -0300

    versão nova

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("nav ul li a");
+    links.forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute("href"));
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+});
