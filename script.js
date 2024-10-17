18782e8cae53204b29b072981ec850b022129048
gabriel-vincius-silva <gabreuvinici@gmail.com>
2024-10-17 13:06:35 -0300

    pagina curriculo funciona corretamebnte

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const links = document.querySelectorAll("nav ul li a");
     links.forEach(link => {
         link.addEventListener("click", function(e) {
-            e.preventDefault();
-            const target = document.querySelector(this.getAttribute("href"));
-            target.scrollIntoView({
-                behavior: "smooth"
-            });
+            // Só aplica a rolagem suave para links internos, não para links que abrem em nova aba
+            if (this.getAttribute("target") !== "_blank") {
+                e.preventDefault();
+                const target = document.querySelector(this.getAttribute("href"));
+                if (target) {
+                    target.scrollIntoView({
+                        behavior: "smooth"
+                    });
+                }
+            }
         });
     });
 });
